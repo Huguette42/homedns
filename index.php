5d0ac1439ded2057789a9709645949aa5bec5bb9
huguette42 <hugojeanselme@gmail.com>
2025-01-07 20:57:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1 class="centered">SIOlaTin <img height="120px" src="siolatin.png"/></h1>
+    <p>Voici nos differentes boulangerie du groupe SIOlaTin</p>
+    <br>
+    <div class="test">
+        <input type="text" id="search" placeholder="Rechercher une boulangerie">
+        <button id="searchButton">Rechercher</button>
+    </div>
+</body>
+</html>
